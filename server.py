04c29d7cb69840ec1e1ee0e107d637598eaf9a53
bagineer <49871110+bagineer@users.noreply.github.com>
2021-02-03 19:20:27 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 # server.py
 import socket
 
-host = '192.168.1.4'    # IP Address
-port = 8080             # Port Number
+host = '192.168.1.4'    # Type your IP Address
+port = 8080             # Type your Port Number
 
 server_sock = socket.socket(socket.AF_INET)
 server_sock.bind((host, port))
