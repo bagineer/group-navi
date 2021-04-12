83ae5d304272203b2d6d7f2409ca0b84e736e57c
bagineer <bih198721@gmail.com>
2021-04-12 16:33:13 +0900

    Receiving Data from client

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 # server.py
 import socket
+import sys
 
-host = '192.168.1.4'    # Type your IP Address
-port = 8080             # Type your Port Number
+host = '192.168.1.4'    # IP Address
+port = 8080             # Port Number
+client_list = []
 
 server_sock = socket.socket(socket.AF_INET)
 server_sock.bind((host, port))
 server_sock.listen(1)
 
+
 print("Waiting for connection...")
 client_sock, addr = server_sock.accept()
 print('Connected by', addr)
+
+while True:
+    data = client_sock.recv(1024)
+    if not data:
+        break
+    print(data.decode())
